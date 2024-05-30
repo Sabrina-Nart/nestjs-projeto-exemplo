bcfa306b46a0162d538f1ed5feeb4ee203caa4a1
Sabrina-Nart <sabrinacominnart@gmail.com>
2024-05-30 16:53:16 -0300

    t

--- src/autor/autor.dto.ts
+++ src/autor/autor.dto.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { GeneroEnum } from "./genero.enum";
 
 export class AutorDto{
@@ -17,3 +18,23 @@ export class AutorDto{
     @isOptional()
     genero:GeneroEnum;
 }
+
+function isOptional(): (target: AutorDto, propertyKey: "id") => void {
+    throw new Error("Function not implemented.");
+}
+function IsUUID(): (target: AutorDto, propertyKey: "id") => void {
+    throw new Error("Function not implemented.");
+}
+
+function IsNotEmpty(): (target: AutorDto, propertyKey: "nome") => void {
+    throw new Error("Function not implemented.");
+}
+
+function IsDateString(): (target: AutorDto, propertyKey: "dataNascimento") => void {
+    throw new Error("Function not implemented.");
+}
+
+function isEnum(GeneroEnum: typeof GeneroEnum): (target: AutorDto, propertyKey: "genero") => void {
+    throw new Error("Function not implemented.");
+}
+
