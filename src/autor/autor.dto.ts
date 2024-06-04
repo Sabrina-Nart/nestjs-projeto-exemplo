b88bfff56d527f32093f91739b2fc48b4dd2ea8f
Sabrina-Nart <sabrinacominnart@gmail.com>
2024-06-03 21:22:33 -0300

    auterado a entidade de autor

--- src/autor/autor.dto.ts
+++ src/autor/autor.dto.ts
@@ -1,29 +1,29 @@
 /* eslint-disable prettier/prettier */
 import {
-        IsDateString,
-        IsEnum,
-        IsNotEmpty,
-        IsOptional,
-        IsString,
-        IsUUID,
+      IsDateString,
+      IsEnum,
+      IsNotEmpty,
+      IsOptional,
+      IsString,
+      IsUUID,
 } from 'class-validator';
 
-  import { GeneroEnum } from './genero.enum';
-  
-  export class AutorDto {
-        @IsUUID()
-        @IsOptional()
-        id: string;
+import { GeneroEnum } from './genero.enum';
     
-        @IsString()
-        @IsNotEmpty()
-        nome: string;
+export class AutorDto {
+      @IsUUID()
+      @IsOptional()
+      id: string;
     
-        @IsDateString()
-        @IsOptional()
-        dataNascimento: Date;
+      @IsString({ message: 'O campo nome deve ser do tipo texto' }) //PERSONALIZAÇÃO
+      @IsNotEmpty({ message: 'O nome não pode ser vazio' })
+      nome: string;
     
-        @IsEnum(GeneroEnum)
-        @IsOptional()
-        genero: GeneroEnum;
-  }
+      @IsDateString() //ESPECIFICAR AS MENSAGENS
+      @IsOptional()
+      dataNascimento: Date;
+    
+      @IsEnum(GeneroEnum, { message: 'O genêrno deve ser apenas M, F ou I.' })
+      @IsOptional()
+      genero: GeneroEnum;
+}
--- src/autor/autor.service.ts
+++ src/autor/autor.service.ts
@@ -1,47 +1,96 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import {
+        BadRequestException,
+        Injectable,
+        NotFoundException,
+} from '@nestjs/common';
+
 import { Repository } from 'typeorm';
 import { AutorEntity } from './autor.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AutorDto } from './autor.dto';
+  
+//No Service ficam as regras de3 negócio
 
 @Injectable()
 export class AutorService {
-  
     constructor(
+
         @InjectRepository(AutorEntity)
         private autorRepository: Repository<AutorEntity>,
     ) {}
-
+    
     findAll() {
         return this.autorRepository.find();
     }
-
+    
     async findById(id: string): Promise<AutorEntity> {
         const findOne = await this.autorRepository.findOne({ where: { id } });
 
         if (!findOne) {
-          throw new NotFoundException('Autor não encontrado com o id ' + id);
+            throw new NotFoundException('Autor não encontrado com o id ' + id);
         }
-        return findOne;
-    }
-
-    async remove(id: string) {
-        const findById = await this.findById(id);
-
-        await this.autorRepository.remove(findById);
-
-        return { ...findById, id };
-    }
-
-    async create(dto: AutorDto) {
-        const newAutor = this.autorRepository.create(dto);
-
-        return this.autorRepository.save(newAutor);
-    }
-
-    async update({ id, ...dto }: AutorDto) {
-        await this.findById(id);
+            return findOne;
+        }
+    
+        async remove(id: string) {
+            const findById = await this.findById(id);
+            await this.autorRepository.remove(findById);
+            return { ...findById, id };
+        }
+    
+        async create(dto: AutorDto) {
+            const newAutor = this.autorRepository.create(dto);
         
-        return this.autorRepository.save({ id, ...dto });
+            this.validateAutor(newAutor);
+        
+            return this.autorRepository.save(newAutor);
+        }
+    
+        async update(autor: AutorDto) {
+            await this.findById(autor.id);
+        
+            this.validateAutor(autor);
+        
+            return this.autorRepository.save(autor);
+        }
+    
+        private validateAutor(autor: AutorEntity | AutorDto) {
+            this.validateAutorNascimento(autor);
+            // this.validateAutorLivros
+        }
+    
+        private validateAutorNascimento(autor: AutorEntity) {
+            const dataAtual = new Date();
+            const dataNascimento = new Date(autor.dataNascimento);
+
+            // if(
+            //   dataAtual.getTime() - dataNascimento.getTime() <
+            //   (1000 * 60 * 60 * 24 * 365 * 18)
+            // ) {
+            //   throw new BadRequestException('O autor deve ter no mínimo 18 anos')
+            // }
+
+            const diferencaAno = dataAtual.getUTCFullYear() - dataNascimento.getUTCFullYear();
+
+            if (diferencaAno < 18) {
+                throw new BadRequestException('O autor deve ter no mínimo 18 anos');
+            } else if (diferencaAno === 18) {
+                const meses = dataAtual.getUTCMonth() + 1 - (dataNascimento.getUTCMonth() + 1);
+
+                if (meses < 0) {
+                    throw new BadRequestException(
+                        `O autor deve ter no mínimo 18 anos (faltam ainda ${meses * -1} mes(es))`,
+                    );
+                } else if (dataAtual.getUTCMonth() - dataNascimento.getUTCMonth() === 0) {
+                    const dias = dataAtual.getUTCDate() - dataNascimento.getUTCDate();
+
+                    if (dias < 0) {
+                        throw new BadRequestException(
+                            `O autor deve ter no mínimo 18 anos (faltam ainda ${dias * -1} dia(s))`,
+                        );
+                    }
+                }
+            }
     }
 }
